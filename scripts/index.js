ffca4340997f63c5b159c336952a384680e67260
Adel Almaleh <adel.almaleh@resolutions.com.my>
2019-03-10 11:00:11 +0100

    Do some calculations.

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,7 @@
-// Int  1     2     3     4     5     6     7     8     9     10    50    100   500   1000
+// INT  	1     2     3     4     5     6     7     8     9     10    50    100   500   1000
 // Roman    I    II    III   IV     V     VI   VII   VIII  IX     X     L     C     D      M
+
+// some calculations: 
+// Int 23 (10+10+3) Rom XXIII
+// Int 36 (10+10+10+5+1) XXXVI
+// Int 999 = (1000-100)+(100-10)+(10-1) = (M-C)+(C-X)+(X-I) = CM+XC+IX = CMXCIX
