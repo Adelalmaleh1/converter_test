c4dca79af9606aef75244d125122ae6e0de13e1b
Adel Almaleh <adel.almaleh@resolutions.com.my>
2019-03-10 10:57:28 +0100

    First comment adding table of numbers

---
+++ scripts/index.js
@@ -0,0 +1,2 @@
+// Int  1     2     3     4     5     6     7     8     9     10    50    100   500   1000
+// Roman    I    II    III   IV     V     VI   VII   VIII  IX     X     L     C     D      M
