21a5adc468512649afd83fd9c015962610b7d99c
Adel Almaleh <adel.almaleh@resolutions.com.my>
2019-03-10 11:19:05 +0100

    Created the int, roman Array in the function.

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,14 @@
 // Int 23 (10+10+3) Rom XXIII
 // Int 36 (10+10+10+5+1) XXXVI
 // Int 999 = (1000-100)+(100-10)+(10-1) = (M-C)+(C-X)+(X-I) = CM+XC+IX = CMXCIX
+
 function func(number) {
+	// Array
+	var int = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  	var roman = ['M', 'CM' ,'D' ,'CD' ,'C' ,'XC' ,'L' ,'XL' ,'X' ,'IX' ,'V' ,'IV' ,'I'];
+  	var result = '';
 
 }
 
+// This is the result.
 document.write(func(36));
